ee6d14090bddcda393b15dfa58866a35268d48a2
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-06-07 05:36:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while array.length < counter do 
-    new_array.push(array[counter])
+    new array = new_array.push(array[counter])
     counter += 1 
   end
   new_array
